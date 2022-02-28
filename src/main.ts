62c02f2547d0b1579348302d664055d852915704
Paul Pacheco <paulpach@gmail.com>
2022-02-28 14:47:17 -0600

    fix title and error

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function run(): Promise<void> {
 **${results.passed} tests passed**
 **${results.failed} tests failed**
 `;
-    core.startGroup("Test Report for ${title}");
+    core.startGroup(`Test Report for ${title}`);
 
     core.info(summary);
 
@@ -66,7 +66,7 @@ async function run(): Promise<void> {
     */
 
     core.endGroup();
-  } catch (error: any) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
